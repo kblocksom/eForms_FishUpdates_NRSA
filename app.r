69473f5baa3d6e42842ee8b32c1678606f6fe09e
Karen Blocksom <blocksom.karen@epa.gov>
2019-10-24 15:29:12 -0700

    Semi-functional now

--- app.r
+++ app.r
@@ -53,7 +53,24 @@ server = function(input, output, session){
   output$MainBody_fish<-renderUI({
     fluidPage(
       hr(),
-      column(12,dataTableOutput("Main_table_fish"))
+      column(6,offset = 6,
+             HTML('<div class="btn-group" role="group" aria-label="Fish Updates" style = "padding:10px">'),
+             ### tags$head() This is to change the color of "Add a new row" button
+             tags$head(tags$style(".butt2{background-color:#231651;} .butt2{color: #e6ebef;}")),
+             div(style="display:inline-block;width:30%;text-align: center;",actionButton(inputId = "Add_row_head",label = "Add", class="butt2") ),
+             tags$head(tags$style(".butt4{background-color:#4d1566;} .butt4{color: #e6ebef;}")),
+             div(style="display:inline-block;width:30%;text-align: center;",actionButton(inputId = "mod_row_head",label = "Edit", class="butt4") ),
+             tags$head(tags$style(".butt3{background-color:#590b25;} .butt3{color: #e6ebef;}")),
+             div(style="display:inline-block;width:30%;text-align: center;",actionButton(inputId = "Del_row_head",label = "Delete", class="butt3") ),
+             ### Optional: a html button 
+             # HTML('<input type="submit" name="Add_row_head" value="Add">'),
+             HTML('</div>') ),
+      
+      column(12,dataTableOutput("Main_table_fish")),
+      tags$script("$(document).on('click', '#Main_table_fish button', function () {
+                  Shiny.onInputChange('lastClickId',this.id);
+                  Shiny.onInputChange('lastClick', Math.random()) });")
+      
     ) 
   })
   
@@ -61,27 +78,156 @@ server = function(input, output, session){
   #### render DataTable part ####
   output$Main_table_fish<-renderDataTable({
     DT=vals_fish$Data
-    datatable(DT,editable = list(target="cell",disable = list(columns = c(1:9))),
-              rownames=F,
-     selection = "none") 
-  }, server=TRUE)
+    datatable(DT,
+              #editable = list(target="cell",disable = list(columns = c(1:9))),
+              rownames=F, 
+              selection = "single",escape=F) 
+  })
   
-  proxy = dataTableProxy('Main_table_fish')
-  
-  observeEvent(input$Main_table_fish_cell_edit, {
-    
-    info = input$Main_table_fish_cell_edit
-    
-    str(info) 
-    i = info$row 
-    j = info$col + 1L
-    v = info$value
-    
-    vals_fish$Data[i, j] <<- DT::coerceValue(v, vals_fish$Data[i, j]) 
-    replaceData(proxy, vals_fish$Data, resetPaging = FALSE) # important
-    
+  observeEvent(input$Add_row_head, {
+    ### This is the pop up board for input a new row
+    showModal(modalDialog(title = "Add a new row",
+                          textInput(paste0("Name_add", input$Add_row_head), "Fish Species"),
+                          textInput(paste0("Count6_add", input$Add_row_head), "Count < 6in length"),
+                          textInput(paste0("Count12_add", input$Add_row_head), "Count 6-12in length"),
+                          textInput(paste0("Count18_add", input$Add_row_head), "Count 12-18in length"),  
+                          textInput(paste0("Count19_add", input$Add_row_head), "Count > 18in length"),
+                          
+                          actionButton("go", "Add row"),
+                          easyClose = TRUE, footer = NULL ))
     
   })
+  ### Add a new row to DT  
+  observeEvent(input$go, {
+    new_row=data.frame(unique(data.frame(vals_fish$Data[,1:9])),
+      NAME_COM=as.character(input[[paste0("Name_add", input$Add_row_head)]] ),
+      COUNT_6=input[[paste0("Count6_add", input$Add_row_head)]],
+      COUNT_12=input[[paste0("Count12_add", input$Add_row_head)]],
+      COUNT_18=input[[paste0("Count18_add", input$Add_row_head)]],
+      COUNT_19=input[[paste0("Count19_add", input$Add_row_head)]]
+      ,stringsAsFactors=F)
+    vals_fish$Data<-gtools::smartbind(vals_fish$Data,new_row)
+    removeModal()
+  })
+  
+  ### delete selected rows part
+  ### this is warning messge for deleting
+  observeEvent(input$Del_row_head,{
+    showModal(
+      if(length(input$Main_table_fish_rows_selected)>=1 ){
+        modalDialog(
+          title = "Warning",
+          paste("Are you sure you want to delete",length(input$Main_table_fish_rows_selected),"rows?" ),
+          footer = tagList(
+            modalButton("Cancel"),
+            actionButton("ok", "Yes")
+          ), easyClose = TRUE)
+      }else{
+        modalDialog(
+          title = "Warning",
+          paste("Please select row(s) that you want to delete!" ),easyClose = TRUE
+        )
+      }
+      
+    )
+  })
+  
+  ### If user say OK, then delete the selected rows
+  observeEvent(input$ok, {
+    vals_fish$Data=vals_fish$Data[-input$Main_table_fish_rows_selected,]
+    removeModal()
+  })
+  
+  
+  ### edit button
+  observeEvent(input$mod_row_head,{
+    showModal(
+      if(length(input$Main_table_fish_rows_selected)>=1 ){
+        modalDialog(
+          fluidPage(
+            h3(strong("Modification"),align="center"),
+            hr(),
+            dataTableOutput('row_modif'),
+            actionButton("save_changes","Save changes"),
+            tags$script(HTML("$(document).on('click', '#save_changes', function () {
+                             var list_value=[]
+                             for (i = 0; i < $( '.new_input' ).length; i++)
+                             {
+                             list_value.push($( '.new_input' )[i].value)
+                             }
+                             Shiny.onInputChange('newValue', list_value) });")) ), size="l" )
+  }else{
+    modalDialog(
+      title = "Warning",
+      paste("Please select the row that you want to edit!" ),easyClose = TRUE
+    )
+  }
+  
+          )
+    })
+  
+  
+  
+  
+  #### modify part
+  output$row_modif<-renderDataTable({
+    selected_row=input$Main_table_fish_rows_selected
+    old_row=vals_fish$Data[selected_row]
+    row_change=list()
+    for (i in colnames(old_row))
+    {
+      if (is.numeric(vals_fish$Data[[i]]))
+      {
+        row_change[[i]]<-paste0('<input class="new_input" value= ','"',old_row[[i]],'"','  type="number" id=new_',i,' ><br>')
+      } 
+      else if( is.Date(vals_fish$Data[[i]])){
+        row_change[[i]]<-paste0('<input class="new_input" value= ','"',old_row[[i]],'"',' type="date" id=new_  ',i,'  ><br>') 
+      }
+      else 
+        row_change[[i]]<-paste0('<input class="new_input" value= ','"',old_row[[i]],'"',' type="textarea"  id=new_',i,'><br>')
+    }
+    row_change=as.data.table(row_change)
+    setnames(row_change,colnames(old_row))
+    DT=row_change
+    DT 
+  },escape=F,options=list(dom='t',ordering=F,scrollX = TRUE),selection="none" )
+  
+  
+  
+  ### This is to replace the modified row to existing row
+  observeEvent(input$newValue,
+               {
+                 newValue=lapply(input$newValue, function(col) {
+                   if (suppressWarnings(all(!is.na(as.numeric(as.character(col)))))) {
+                     as.numeric(as.character(col))
+                   } else {
+                     col
+                   }
+                 })
+                 DF=data.frame(lapply(newValue, function(x) t(data.frame(x))))
+                 colnames(DF)=colnames(vals_fish$Data)
+                 vals_fish$Data[input$Main_table_fish_rows_selected]<-DF
+                 
+               }
+  )
+  
+  
+  # proxy = dataTableProxy('Main_table_fish')
+  # 
+  # observeEvent(input$Main_table_fish_cell_edit, {
+  #   
+  #   info = input$Main_table_fish_cell_edit
+  #   
+  #   str(info) 
+  #   i = info$row 
+  #   j = info$col + 1L
+  #   v = info$value
+  #   
+  #   vals_fish$Data[i, j] <<- DT::coerceValue(v, vals_fish$Data[i, j]) 
+  #   replaceData(proxy, vals_fish$Data, resetPaging = FALSE) # important
+  #   
+  #   
+  # })
   
   
   # ### save to RDS part 
